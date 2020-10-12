826466fc64ea758e86bb8a03f5678e44e0c2d474
Oleh Zahorulko <oleh.zaho@hotmail.com>
2020-10-12 14:00:26 +0200

    initial commit

---
+++ capture.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+const mkdirp = require('mkdirp');
+
+async function main() {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    let page = await browser.newPage();
+
+    page.setViewport({ width: 640, height: 680 });
+    await page.goto(`file://home/olehz/Desktop/tests/reg-puppeteer-demo-with-minio/index.html`);
+    await new Promise(res => setTimeout(() => res(), 300));
+    await page.screenshot({ path: 'screenshot/index.png' });
+
+    await page.close();
+    await browser.close();
+}
+
+mkdirp.sync('screenshot');
+main();
